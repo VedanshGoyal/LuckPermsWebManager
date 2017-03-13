9f6c8c3691aec4cfe4129e04ce96b3cdb6b7f903
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-03-12 23:03:13 -0400

    Adding new configurations.

---
+++ config/halfpetal.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'footer' => [
+        'show_copyright' => env('HALFPETAL_COPYRIGHT', true),
+        'show_github' => env('HALFPETAL_GITHUB', true),
+        'show_social' => env('HALFPETAL_SOCIAL', true),
+    ]
+];
---
+++ config/luckperms.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /***
+     * This is all the configuration for the public information,
+     * all of this information will be showed to logged in users
+     * by default.
+     */
+    'show_action_log' => false,
+
+    'groups' => [
+        'show_permissions' => true,
+        'show_members' => true,
+    ],
+
+    'users' => [
+        'show_users' => true,
+        'show_user_permissions' => true,
+        'show_groups' => true,
+        'show_prefix' => true,
+    ]
+]; 
